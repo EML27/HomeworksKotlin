0cb66548b7619ee0e16f7bfb5de7655ad280c468
Emil <emil.73@mail.ru>
2019-10-22 08:48:51 +0300

    init

---
+++ app/src/main/java/com/example/homeworkskotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.homeworkskotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        go_button.setOnClickListener{
+            intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
